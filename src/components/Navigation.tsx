90782759b705ac7b5e738531cd7df3b9b78f7087
BANER <86590991+BANSAFAn@users.noreply.github.com>
2025-06-10 16:14:37 +0300

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useLanguage } from '@/context/LanguageContext';
 import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
   NavigationMenuLink,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
@@ -11,32 +14,44 @@ export const Navigation = () => {
   const { t } = useLanguage();
 
   return (
-    <div className="flex justify-center space-x-4 mb-8">
-      <Link to="/">
-        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-          {t('home')}
-        </NavigationMenuLink>
-      </Link>
-      <Link to="/about">
-        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-          {t('about')}
-        </NavigationMenuLink>
-      </Link>
-      <Link to="/projects">
-        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-          {t('projects')}
-        </NavigationMenuLink>
-      </Link>
-      <Link to="/posts">
-        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-          {t('posts')}
-        </NavigationMenuLink>
-      </Link>
-      <Link to="/blog">
-        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-          {t('blog')}
-        </NavigationMenuLink>
-      </Link>
-    </div>
+    <NavigationMenu className="flex justify-center mb-8">
+      <NavigationMenuList className="space-x-4">
+        <NavigationMenuItem>
+          <Link to="/">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              {t('home')}
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link to="/about">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              {t('about')}
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link to="/projects">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              {t('projects')}
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link to="/posts">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              {t('posts')}
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link to="/blog">
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              {t('blog')}
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
   );
 };
