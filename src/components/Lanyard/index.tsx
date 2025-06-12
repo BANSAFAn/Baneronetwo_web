187793641160e99ed2dfa2e173ab82a48942c6f4
BANER <86590991+BANSAFAn@users.noreply.github.com>
2025-06-12 09:19:39 +0300

    deleting

--- src/components/Lanyard/index.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { lazy } from "react";
-
-// Используем lazy для асинхронной загрузки компонента
-const LanyardComponent = lazy(() => import('./Lanyard'));
-
-export default LanyardComponent;
